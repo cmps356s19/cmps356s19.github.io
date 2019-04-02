c1f0b27fd4d2f623a9f9fb760e40773269d22d92
ae <erradi@live.com>
2019-04-02 15:19:02 +0300

    Hifz App

---
+++ Midterm-HifzApp-Female/repositories/HifzRepository-Test.js
@@ -0,0 +1,1 @@
+let hifzRepo = require('./HifzRepository');
---
+++ Midterm-HifzApp-Female/repositories/HifzRepository.js
@@ -0,0 +1,10 @@
+const fs = require('fs-extra');
+const path = require('path');
+const surahFilePath = path.join(__dirname, '../data/surah.json');
+const hifzFilePath = path.join(__dirname, '../data/hifz-register.json');
+
+class HifzRepository {
+    //ToDo: Implement Hifz Repository
+}
+
+module.exports = new HifzRepository();
